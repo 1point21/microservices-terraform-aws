9171b594b627578dcaa1b023d387930cae7e4818
1point21 <j.drinkall@mailfence.com>
2023-12-04 10:36:55 +0000

    feat: copy required variables from vpc module

---
+++ variables.tf
@@ -0,0 +1,26 @@
+# PROJECT VARIABLES
+variable "project_name" {
+  description = "name of the overall project"
+  type = string
+}
+
+# VPC VARIABLES
+variable "vpc_cidr" {
+  description = "cidr block value for the main vpc"
+  type = string
+}
+
+variable "public_subnet_cidr" {
+  description = "list of strings of cidr block ranges for public subnets"
+  type = list(string)
+}
+
+variable "private_subnet_cidr" {
+  description = "list of strings of the cidr block ranges for private subnets"
+  type = list(string)
+}
+
+variable "azs" {
+    description = "list of strings of the availability zones for subnet creation"
+    type = list(string)
+}
