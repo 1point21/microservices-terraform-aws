603127b976e2a628cf1427cc2c32423a39142593
1point21 <j.drinkall@mailfence.com>
2023-12-04 10:36:17 +0000

    feat: outputs list of ids of public subnets

--- modules/vpc/outputs.tf
+++ modules/vpc/outputs.tf
@@ -2,3 +2,8 @@ output "vpc_id" {
     description = "id of the main vpc"
     value = aws_vpc.main.id
 }   
+
+output "pub_sub_ids" {
+  description = "list of ids of the public subnets"
+  value = aws_subnet.public_subnet[*].ids
+}
