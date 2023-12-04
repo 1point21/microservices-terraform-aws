1855da54f1941916cc49e4f196e4cfbd73050f4c
1point21 <j.drinkall@mailfence.com>
2023-12-04 16:56:29 +0000

    feat: add variables required to resource creation

---
+++ modules/heating/variables.tf
@@ -0,0 +1,21 @@
+# PROJECT VARIABLES
+variable "project_name" {
+  description = "name of the overall project"
+  type        = string
+}
+
+# INSTANCE VARIABLES
+variable "key_name" {
+  description = "name of the key pair for ssh access to ec2"
+  type        = string
+}
+
+variable "security_group_ids" {
+  description = "a list of strings of the ids of the security groups"
+  type        = list(string)
+}
+
+variable "heating_subnet_id" {
+  description = "id of the subnet the heating instance should be launched into"
+  type        = string
+}
