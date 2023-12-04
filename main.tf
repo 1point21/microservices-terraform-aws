1fc013516ad7d3e4596050e9cbd9d79e9f2b3775
1point21 <j.drinkall@mailfence.com>
2023-12-04 11:42:55 +0000

    feat: creates resources by calling security module

--- main.tf
+++ main.tf
@@ -8,3 +8,11 @@ module "vpc" {
   private_subnet_cidr = var.private_subnet_cidr
   azs = var.azs
 }
+
+# CREATE SECURITY GROUPS AND RULES
+module "security" {
+  source = "./modules/security"
+
+  project_name = var.project_name
+  vpc_id = module.vpc.vpc_id
+}
