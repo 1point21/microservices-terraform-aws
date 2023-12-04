8381c0b1f6657857d800aff0a3926f9a8d5ceb70
1point21 <j.drinkall@mailfence.com>
2023-12-04 10:33:30 +0000

    feat: adds required variables for resource creation

--- modules/vpc/variables.tf
+++ modules/vpc/variables.tf
@@ -9,3 +9,19 @@ variable "vpc_cidr" {
   description = "cidr block value for the main vpc"
   type = string
 }
+
+variable "public_subnet_cidr" {
+  description = "list of strings of cidr block ranges for public subnets"
+  type = list(string)
+}
+
+variable "private_subnet_cidr" {
+  description = "list of strings of the cidr block ranges for private subnets"
+  type = list(string)
+}
+
+variable "azs" {
+    description = "list of strings of the availability zones for subnet creation"
+    type = list(string)
+}
+
