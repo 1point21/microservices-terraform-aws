aeefb6e052bf1dbce640819f5376728c756f4b72
1point21 <j.drinkall@mailfence.com>
2023-12-04 15:41:42 +0000

    feat: calls lighting module and creates lighting server and db

--- main.tf
+++ main.tf
@@ -2,11 +2,11 @@
 module "vpc" {
   source = "./modules/vpc"
 
-  project_name = var.project_name
-  vpc_cidr = var.vpc_cidr
-  public_subnet_cidr = var.public_subnet_cidr
+  project_name        = var.project_name
+  vpc_cidr            = var.vpc_cidr
+  public_subnet_cidr  = var.public_subnet_cidr
   private_subnet_cidr = var.private_subnet_cidr
-  azs = var.azs
+  azs                 = var.azs
 }
 
 # CREATE SECURITY GROUPS AND RULES
@@ -14,5 +14,15 @@ module "security" {
   source = "./modules/security"
 
   project_name = var.project_name
-  vpc_id = module.vpc.vpc_id
+  vpc_id       = module.vpc.vpc_id
+}
+
+# CREATE LIGHTING SERVER AND DB
+module "lighting" {
+  source = "./modules/lighting"
+
+  project_name       = var.project_name
+  key_name           = var.key_name
+  security_group_ids = module.security.security_group_ids
+  lighting_subnet_id = module.vpc.pub_sub_ids[0]
 }
