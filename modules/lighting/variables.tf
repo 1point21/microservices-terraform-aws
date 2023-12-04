ccdc58ae933e199a9ce1bf1b50a3b68a1ab05058
1point21 <j.drinkall@mailfence.com>
2023-12-04 14:14:39 +0000

    feat: defining variables required for resource creationg

---
+++ modules/lighting/variables.tf
@@ -0,0 +1,26 @@
+# PROJECT VARIABLES
+variable "project_name" {
+  description = "name of the overall project"
+  type = string
+}
+
+# INSTANCE VARIABLES
+variable "key_name" {
+  description = "name of the key pair for ssh access to ec2"
+  type = string
+}
+
+variable "security_group_ids" {
+  description = "a list of strings of the ids of the security groups"
+  type = list(string)
+}
+
+variable "lighting_subnet_id" {
+  description = "id of the subnet the lighting instance should be launched into"
+  type = string
+}
+
+variable "instance_profile" {
+  description = "string value of IAM role to be attached to the ec2 instance"
+  type = string
+}
