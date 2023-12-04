4a87704e5424b824a6dd5439ea2e2575313d91e2
1point21 <j.drinkall@mailfence.com>
2023-12-04 11:42:11 +0000

    fix: removing https rule temporarily

--- modules/security/main.tf
+++ modules/security/main.tf
@@ -9,6 +9,11 @@ resource "aws_security_group" "sg_allow_ingress" {
   name        = "allow_ingress"
   description = "allow selected ingress traffic"
   vpc_id      = var.vpc_id
+
+  tags = {
+    Name = "${var.project_name}-sg-igress"
+    ManagedBy = "Terraform"
+  }
 }
 
 # INGRESS RULES
@@ -22,16 +27,17 @@ resource "aws_security_group_rule" "http" {
   security_group_id = aws_security_group.sg_allow_ingress.id
 }
 
-resource "aws_security_group_rule" "https" {
-  type = "ingress"
-  # OBS: ports should be 443 - maybe on refactor
-  from_port         = 80
-  to_port           = 80
-  protocol          = "tcp"
-  cidr_blocks       = ["0.0.0.0/0"]
-  ipv6_cidr_blocks  = ["::/0"]
-  security_group_id = aws_security_group.sg_allow_ingress.id
-}
+# COMMENTED OUT ATM SO THAT I DON'T NEED TO DEAL WITH CERTIFICATES
+
+# resource "aws_security_group_rule" "https" {
+#   type = "ingress"
+#   from_port         = 443
+#   to_port           = 443
+#   protocol          = "tcp"
+#   cidr_blocks       = ["0.0.0.0/0"]
+#   ipv6_cidr_blocks  = ["::/0"]
+#   security_group_id = aws_security_group.sg_allow_ingress.id
+# }
 
 resource "aws_security_group_rule" "ssh" {
   type              = "ingress"
@@ -39,7 +45,7 @@ resource "aws_security_group_rule" "ssh" {
   to_port           = 22
   protocol          = "tcp"
   cidr_blocks       = ["${chomp(data.http.myipaddr.response_body)}/32"]
-  security_group_id = aws_security_group.allow_ssh.id
+  security_group_id = aws_security_group.sg_allow_egress.id
 }
 
 ### EGRESS
@@ -49,6 +55,11 @@ resource "aws_security_group" "sg_allow_egress" {
   name        = "allow_egress"
   description = "allow selected egress traffic"
   vpc_id      = var.vpc_id
+
+  tags = {
+    Name = "${var.project_name}-sg-egress"
+    ManagedBy = "Terraform"
+  }
 }
 
 # EGRESS RULES
