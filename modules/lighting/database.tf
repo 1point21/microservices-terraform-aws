85e7962c5d19b639390ff7d25772c63ebde5a84b
1point21 <j.drinkall@mailfence.com>
2023-12-04 15:36:47 +0000

    feat: creates table in dynamoDB for lighting server

--- modules/lighting/database.tf
+++ modules/lighting/database.tf
@@ -1,6 +1,14 @@
 # TABLE CREATION
 resource "aws_dynamodb_table" "lighting_table" {
   name = "${var.project_name}-lighting-table"
+    read_capacity = 20
+    write_capacity = 20
+    hash_key = "id"
+
+  attribute {
+    name = "id"
+    type = "N"
+  }
 
   tags = {
     Name      = "${var.project_name}-lighting-table"
