d80db79c04d6dac089f48f4bf74e99691d8238d7
1point21 <j.drinkall@mailfence.com>
2023-12-04 15:39:56 +0000

    fix: minor changes to outputs for security group ids

--- modules/security/outputs.tf
+++ modules/security/outputs.tf
@@ -1,9 +1,9 @@
 output "security_group_ids" {
   description = "a list of strings of security group ids"
   value = [
-    aws_security_group.allow_http.id,
+    aws_security_group.sg_allow_http.id,
     aws_security_group.sg_allow_https.id,
     aws_security_group.sg_allow_ssh.id,
-    aws_security_group.sg_allow_egress
+    aws_security_group.sg_allow_egress.id
   ]
 }
