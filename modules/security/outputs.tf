15e106053d6573953458fb43c84bf0a5e551a2a4
1point21 <j.drinkall@mailfence.com>
2023-12-05 14:50:44 +0000

    feat: add output for security groups for loadbalancer

--- modules/security/outputs.tf
+++ modules/security/outputs.tf
@@ -7,3 +7,8 @@ output "security_group_ids" {
     aws_security_group.sg_allow_egress.id
   ]
 }
+
+output "lb_security_group_ids" {
+  description = "a list of strings of the security groups to be applied to the load balancer"
+  value = [aws_security_group.sg_allow_http.id, aws_security_group.sg_allow_https.id, aws_security_group.sg_allow_egress.id]
+}
