79fd3bcc3b44a6bfde78f0d5c4c8a884ac6c7994
1point21 <j.drinkall@mailfence.com>
2023-12-05 11:40:39 +0000

    feat: adds outputs from module

--- modules/heating/outputs.tf
+++ modules/heating/outputs.tf
@@ -1,11 +1,11 @@
-output "ec2" {
+output "ec2_heating" {
   description = "details of ec2 for heating server"
   value = { id : aws_instance.ec2_heating.id
     public_ip : aws_instance.ec2_heating.public_ip
   public_dns : aws_instance.ec2_heating.public_dns }
 }
 
-output "lighting_table" {
-  description = "name of the DynamoDB table for lighting"
+output "heating_table" {
+  description = "name of the DynamoDB table for heating"
   value       = aws_dynamodb_table.heating_table.name
 }
