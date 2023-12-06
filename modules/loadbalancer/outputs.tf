3fccea3df656ebf3647cba36c38cf4c9e01a04c1
1point21 <j.drinkall@mailfence.com>
2023-12-06 13:31:00 +0000

    fix: changes outputs for integration with new autoscaling module

--- modules/loadbalancer/outputs.tf
+++ modules/loadbalancer/outputs.tf
@@ -2,3 +2,8 @@ output "lb_public_dns" {
   description = "public dns of the load balancer"
   value = aws_lb.alb.dns_name
 }
+
+output "target_arns"{
+  description = "list of arns of the created target groups"
+  value = [aws_lb_target_group.status_tg.arn, aws_lb_target_group.status_tg.arn, aws_lb_target_group.heating_tg.arn,]
+}
--- modules/security/outputs.tf
+++ modules/security/outputs.tf
@@ -12,3 +12,4 @@ output "lb_security_group_ids" {
   description = "a list of strings of the security groups to be applied to the load balancer"
   value = [aws_security_group.sg_allow_http.id, aws_security_group.sg_allow_https.id, aws_security_group.sg_allow_egress.id]
 }
+
