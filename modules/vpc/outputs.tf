69775bcdb7df1c3b1c0a7a2a9eadad1328b575b6
1point21 <j.drinkall@mailfence.com>
2023-12-06 13:31:26 +0000

    fix: changes outputs for integration with new autoscaling module

--- modules/vpc/outputs.tf
+++ modules/vpc/outputs.tf
@@ -7,3 +7,8 @@ output "pub_sub_ids" {
   description = "list of ids of the public subnets"
   value       = aws_subnet.public_subnet[*].id
 }
+
+output "asg_pub_subs" {
+  description = "list of ids of public subnets for use in auto-scaling"
+  value = [aws_subnet.public_subnet[1].id, aws_subnet.public_subnet[2].id ]
+}
