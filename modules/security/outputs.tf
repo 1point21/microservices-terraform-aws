6d1d882ddfa95fcec61948aacd2d7f0aeb7686b3
1point21 <j.drinkall@mailfence.com>
2023-12-06 15:16:33 +0000

    style: terraform format

--- modules/security/outputs.tf
+++ modules/security/outputs.tf
@@ -10,6 +10,6 @@ output "security_group_ids" {
 
 output "lb_security_group_ids" {
   description = "a list of strings of the security groups to be applied to the load balancer"
-  value = [aws_security_group.sg_allow_http.id, aws_security_group.sg_allow_https.id, aws_security_group.sg_allow_egress.id]
+  value       = [aws_security_group.sg_allow_http.id, aws_security_group.sg_allow_https.id, aws_security_group.sg_allow_egress.id]
 }
 
