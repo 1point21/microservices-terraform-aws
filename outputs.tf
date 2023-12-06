ec8282db87293bb67e5dcc32ee0b2d29bb233582
1point21 <j.drinkall@mailfence.com>
2023-12-06 15:18:56 +0000

    refactor: based on module refactor

--- outputs.tf
+++ outputs.tf
@@ -1,24 +1,14 @@
-output "lighting_ec2" {
-  value = module.lighting.ec2
+output "servers" {
+  description = "a list of objects with information about the servers"
+  value       = module.servers.ec2s
 }
 
-output "table_name" {
-  value = module.lighting.lighting_table
-}
-
-output "heating_ec2" {
-  value = module.heating.ec2_heating
-}
-
-output "heating_table" {
-  value = module.heating.heating_table
-}
-
-output "status_ec2" {
-  value = module.status.ec2_status
+output "ec2_ids" {
+  description = "list of the ids of the servers created, in order as services passed in"
+  value       = module.servers.ec2_ids
 }
 
 output "lb_public_dns" {
   description = "public dns of the load balancer"
-  value = module.lb.lb_public_dns
+  value       = module.lb.lb_public_dns
 }
